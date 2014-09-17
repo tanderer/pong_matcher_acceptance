ccbba49160128d9f6f4f64b7e0323a07f46276e4
Andrew Bruce <andrew@camelpunch.com>
2014-09-17 13:20:36 +0100

    Entering a result
    A player should not be matched with someone they've just played.

--- pong_matcher_acceptance_test.rb
+++ pong_matcher_acceptance_test.rb
@@ -9,21 +9,47 @@ class PongMatcherAcceptance < Minitest::Test
   end
 
   def test_that_lonely_player_cannot_be_matched
-    @client = Client.new(@host, "williams")
-    match_request = @client.request_match
+    @williams = Client.new(@host, "williams")
+    match_request = @williams.request_match
 
     refute match_request.fulfilled?, "a single player shouldn't be matched"
   end
 
   def test_that_two_players_can_be_matched
-    @client_1 = Client.new(@host, "williams")
-    @client_2 = Client.new(@host, "sharapova")
+    @williams = Client.new(@host, "williams")
+    @sharapova = Client.new(@host, "sharapova")
 
-    request_1 = @client_1.request_match
-    request_2 = @client_2.request_match
+    request_1 = @williams.request_match
+    request_2 = @sharapova.request_match
 
     assert request_1.fulfilled?, "williams didn't receive notification of her match!"
     assert request_2.fulfilled?, "sharapova didn't receive notification of her match!"
+  end
+
+  def test_that_entering_result_ensures_match_with_new_player
+    @williams = Client.new(@host, "williams")
+    @sharapova = Client.new(@host, "sharapova")
+    @navratilova = Client.new(@host, "navratilova")
+
+    williams_request_id = SecureRandom.uuid
+
+    williams_request = @williams.request_match(match_request_id: williams_request_id)
+    @sharapova.request_match
+
+    @williams.loses_to(@sharapova, match_id: williams_request.match_id)
+
+    williams_new_request = @williams.request_match
+    sharapova_new_request = @sharapova.request_match
+    navratilova_request = @navratilova.request_match
+
+    assert williams_new_request.fulfilled?,
+      "Williams didn't receive notification of her match!"
+
+    refute sharapova_new_request.fulfilled?,
+      "Sharapova just played Williams! Expected Navratilova to be matched with Williams."
+
+    assert navratilova_request.fulfilled?,
+      "Navratilova didn't receive notification of her match!"
   end
 end
 
@@ -40,6 +66,8 @@ class Admin
 end
 
 class Client
+  attr_reader :id
+
   def initialize(host, id)
     @http = Faraday.new(url: host)
     @id = id
@@ -49,9 +77,25 @@ class Client
     MatchRequest.new(match_request_id, http, id).tap(&:call)
   end
 
+  def loses_to(winner, options)
+    enter_result(
+      match_id: options.fetch(:match_id),
+      winner: winner,
+      loser: self
+    )
+  end
+
   private
 
-  attr_reader :http, :id
+  def enter_result(match_id: nil, winner: nil, loser: nil)
+    if [match_id, winner].any?(&:nil?)
+      raise ArgumentError, "One of match_id: #{match_id}, winner: #{winner} is nil!"
+    else
+      http.post("/results", JSON.generate(match_id: match_id, winner: winner.id, loser: loser.id))
+    end
+  end
+
+  attr_reader :http
 end
 
 require "securerandom"
@@ -67,10 +111,14 @@ class MatchRequest
     http.put(path, JSON.generate(player: player_id))
   end
 
-  def fulfilled?(debug = false)
+  def fulfilled?
     response = get(path)
-    pp JSON.parse(response.body) if debug && response.status == 404
     response.status == 200 && has_match_id?(response)
+  end
+
+  def match_id
+    response = get(path)
+    extract(response, "match_id")
   end
 
   private
@@ -83,16 +131,12 @@ class MatchRequest
     end
   end
 
-  def match_id_from_response(response)
-    extract(response, "match_id")
-  end
-
   def extract(response, attribute)
     JSON.parse(response.body)[attribute]
   end
 
   def has_match_id?(response)
-    match_id = match_id_from_response(response)
+    match_id = extract(response, "match_id")
     match_id != "" && !match_id.nil?
   end
 
