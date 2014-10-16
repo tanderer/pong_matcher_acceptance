ba04df7a668f76174d08a95e1ffaabc82da75bcc
Andrew Bruce <andrew@camelpunch.com>
2014-10-16 13:11:38 +0000

    Attempt to pull image before building

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,6 @@
 
 set -ex
 
+docker pull docker.gocd.cf-app.com:5000/pong-matcher-acceptance
 docker build -t docker.gocd.cf-app.com:5000/pong-matcher-acceptance .
 docker run -e "HOST=$HOST" docker.gocd.cf-app.com:5000/pong-matcher-acceptance
