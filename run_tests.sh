4de80b2301952d36557897b262bcbbe67b42fcad
Andrew Bruce <andrew@camelpunch.com>
2014-10-16 13:18:22 +0000

    Cope with missing image, push after build.

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
-docker pull docker.gocd.cf-app.com:5000/pong-matcher-acceptance
+docker pull docker.gocd.cf-app.com:5000/pong-matcher-acceptance || true
 docker build -t docker.gocd.cf-app.com:5000/pong-matcher-acceptance .
+docker push docker.gocd.cf-app.com:5000/pong-matcher-acceptance
 docker run -e "HOST=$HOST" docker.gocd.cf-app.com:5000/pong-matcher-acceptance
