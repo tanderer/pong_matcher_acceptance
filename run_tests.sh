8802f46a57458c85ba8a858a44075f16d4a98541
Andrew Bruce <andrew@camelpunch.com>
2014-11-11 11:41:11 +0000

    Allow insertion of docker build options

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,6 @@
 set -ex
 
 docker pull docker.gocd.cf-app.com:5000/pong-matcher-acceptance || true
-docker build -t docker.gocd.cf-app.com:5000/pong-matcher-acceptance .
+docker build $DOCKER_BUILD_OPTS -t docker.gocd.cf-app.com:5000/pong-matcher-acceptance .
 docker push docker.gocd.cf-app.com:5000/pong-matcher-acceptance
 docker run -e "HOST=$HOST" docker.gocd.cf-app.com:5000/pong-matcher-acceptance
